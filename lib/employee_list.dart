f02a5ed0aac3169c06dc8001acc97a6b4c132e57
keanu <keanujalerr.kj@gmail.com>
2022-10-26 19:21:43 +0700

    fix reload data employee_list

--- lib/employee_list.dart
+++ lib/employee_list.dart
@@ -32,7 +32,7 @@ class _EmployeeListState extends State<EmployeeList> {
 
   FutureOr reloadDataEmployee(dynamic value) {
     setState(() {
-      employee = employee;
+      selectAllEmployee();
     });
   }
 
